c2ac852acd71bff762f0901e1b59c9cffc59d80b
priyankagari <developerpriya789@gmail.com>
2022-06-26 22:45:45 +0100

    first commit

---
+++ src/main/java/com/healthservice/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.healthservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.healthservice.models.ERole;
+import com.healthservice.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
